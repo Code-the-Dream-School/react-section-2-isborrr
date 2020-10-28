8e1e9dcd79eed537d408c93371aa154a3818c2de
Ismael Boro <ismaelboro@Ismaels-MBP.home>
2020-10-27 23:34:09 -0400

    Updates

---
+++ precache-manifest.d77e22c582c5d06e4678add0fbdebf29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52f73a19c5d46744cb5b562dabfd120",
+    "url": "/react-section-2-isborrr/index.html"
+  },
+  {
+    "revision": "c03cf6385b0319746bf0",
+    "url": "/react-section-2-isborrr/static/css/main.fdf28208.chunk.css"
+  },
+  {
+    "revision": "c6cd186d597e8254285c",
+    "url": "/react-section-2-isborrr/static/js/2.91f34e12.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-section-2-isborrr/static/js/2.91f34e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03cf6385b0319746bf0",
+    "url": "/react-section-2-isborrr/static/js/main.1ad580ad.chunk.js"
+  },
+  {
+    "revision": "9f5c0b29e15c3bc1f594",
+    "url": "/react-section-2-isborrr/static/js/runtime-main.e36de61a.js"
+  },
+  {
+    "revision": "b2317448a6ca9069cee581d1b91713b8",
+    "url": "/react-section-2-isborrr/static/media/graduation.b2317448.jpg"
+  },
+  {
+    "revision": "7638681598696e8c62e866327eda0cfd",
+    "url": "/react-section-2-isborrr/static/media/image.76386815.jpg"
+  },
+  {
+    "revision": "2abdf5d0354d31da56375efb10662613",
+    "url": "/react-section-2-isborrr/static/media/linkdln.2abdf5d0.png"
+  },
+  {
+    "revision": "71f3d9c143014929774ecfd093556899",
+    "url": "/react-section-2-isborrr/static/media/mail.71f3d9c1.png"
+  }
+]);
